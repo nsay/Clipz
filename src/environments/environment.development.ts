a54e79c84d9d573ef4e2bbc186eeaca291cdc841
nsay <narithsay@gmail.com>
2023-11-30 21:58:46 -0500

    setup firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAiBjvKWef5c0w3E1XltQE0Y1pFC0580io",
+    authDomain: "clipz-d12f3.firebaseapp.com",
+    projectId: "clipz-d12f3",
+    storageBucket: "clipz-d12f3.appspot.com",
+    appId: "1:535614874214:web:1cdcb002f7b49c371c43a7"
+  }, 
+};
