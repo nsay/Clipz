77aa022060f4001f10ad1999e5eeaa629f06cf1f
nsay <narithsay@gmail.com>
2023-12-02 11:39:39 -0500

    added firebase auth and store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,13 +14,14 @@ import { environment } from 'src/environments/environment';
   declarations: [
     AppComponent,
     NavComponent
-  ],
+  ], 
   imports: [
     BrowserModule,
     AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth  } from '@angular/fire/compat/auth';
+import { AngularFirestore  } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-register',
@@ -39,10 +41,38 @@ export class RegisterComponent {
   showAlert = false;
   alertMsg = 'Please wait! Your account is being created.';
   alertColor = 'blue';
+  inSubmission = false;
 
-  register(): void {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore){}
+
+  async register(): Promise<void> {
     this.showAlert = true;
-    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertMsg = 'Please wait! Your acc ount is being created.';
     this.alertColor = 'blue'
+    this.inSubmission = true;
+
+    let { name, email, age, password, phoneNumber } = this.registerForm.value;
+
+    try {
+      let userCred = await this.auth.createUserWithEmailAndPassword(email as string, password as string)
+
+      //create users collection so we can store user details (except password) in firestore database
+      await this.db.collection('users').add({
+        name: name,
+        email: email,
+        age: age,
+        phoneNumber: phoneNumber
+      }) 
+    } catch (e){  // when email (or invalid email) has already been used
+      this.showAlert = true;
+      this.alertMsg = 'An unexpected error occurred. Please try again later.';
+      this.alertColor = 'red';
+      return
+    }
+
+    //success prompts 
+    this.alertMsg = 'Success! Your account has been created.';
+    this.alertColor = 'green';
+    this.inSubmission = false;
   }
 }
